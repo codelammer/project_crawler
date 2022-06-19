bbc5284fc93fce972467525fa956c44ee681748e
codelammer <briancheruiyott@gmail.com>
2022-06-19 23:45:45 +0300

    let there be parser.js

--- parser.js
+++ parser.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
+const moment = require('moment');
+// const app = require('express')();
 
-const parser = () => {    
+const parser = async (fileLocation) => {    
+    let cases = [];
+    let myData;
+    try {
+        myData = await fs.readFileSync(fileLocation, "utf8");//read html file as string
+    } catch (error) {
+        console.log('error reading the file');
+        return;
+    }
 
-    fs.readFile("ref_files/1.html", "utf8", (err, data) => {      
-        if (err) {
-            console.log(`error reading the file`);
-            console.log(err);
-        }  
-        console.log("Here's your data");
-        const $ = cheerio.load(data);
-        $('h2').each(function () {
-            console.log($(this));
-        });
+    const $ = cheerio.load(myData); //load html file as dom like
+    $('.post').each(function () {
+        //for every post class ('contains the required data)
+        let cheerioTitle = $(this).find('table tr td h2').text().trim(); 
+        let cheerioUrl = $(this).find('.show-more.pull-right').attr('href');
+        let tempDate = $(this).find('table tr td.date-delivered').html().split('<span class="meta_title">Date Delivered: </span>')[1];
+        let cheerioDate = moment(tempDate, 'DD MMM YYYY').format("YYYYMMDD"); //parse date with moment to YYYYMMDD        
+        cases.push({ title: cheerioTitle, url: cheerioUrl, date: cheerioDate });
     });
+    console.log(cases);
 }
 
-parser();
-
-// module.exports = parser;
+parser("ref_files/1.html");
